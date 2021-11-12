abf0d7d318b2f456a5ae1e0da99a134b8a7d98ab
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-12 15:10:10 -0800

    passed seventh test, calculator finds the number of years left based on age and planet

--- src/calculator.js
+++ src/calculator.js
@@ -22,4 +22,22 @@ export class Calculator {
   calculateLifeExpectancy(daysOfExercisePerWeek, isSmoker, heightInches, weightPounds) {
     this.lifeExpectancy = Math.floor(80 * (0.75 + daysOfExercisePerWeek / 14) + (25 - (703 * weightPounds / (heightInches * heightInches))) - (isSmoker * 10));
   }
+  
+  yearsRemaining(planet) {
+    if (planet === 0) {
+      return this.lifeExpectancy - this.earthYears;
+    }
+    else if (planet === 1) {
+      return this.mercuryYears(this.lifeExpectancy - this.earthYears);
+    }
+    else if (planet === 2) {
+      return this.venusYears(this.lifeExpectancy - this.earthYears);
+    }
+    else if (planet === 3) {
+      return this.marsYears(this.lifeExpectancy - this.earthYears);
+    }
+    else {
+      return this.jupiterYears(this.lifeExpectancy - this.earthYears);
+    }
+  }
 }
