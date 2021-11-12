3c2e1787675b8287f3a74db1e7e9888f986f697e
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-12 11:19:31 -0800

    passed fourth test, Calculator converts age to Mars years

--- src/calculator.js
+++ src/calculator.js
@@ -3,5 +3,6 @@ export class Calculator {
     this.earthYears = age;
     this.mercuryYears = this.earthYears * 0.24;
     this.venusYears = this.earthYears * 0.62;
+    this.marsYears = this.earthYears * 1.88;
   }
 }
